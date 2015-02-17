5dc1dd9d8280da771def2050008a71e8ca359884
mitchellFlorman <flormanm@yahoo.com>
2015-02-16 20:17:12 -0500

    First Commit

---
+++ CityGuide/src/net/androidbootcamp/cityguide/Wharf.java
@@ -0,0 +1,13 @@
+package net.androidbootcamp.cityguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Wharf extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.wharf);
+}
+}
